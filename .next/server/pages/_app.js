8ec6ff568656b463d219a8acd495e119b0a90043
Igor Dolzhenkov <igordolzhenkov@Igors-MacBook-Pro-3.local>
2023-02-15 17:46:57 +0300

    Save

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
  * ATTENTION: An "eval-source-map" devtool has been used.
  * This devtool is neither made for production nor for readable output files.
@@ -18,18 +19,17 @@ exports.modules = {
   \****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ App)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @/styles/globals.css */ \"./src/styles/globals.css\");\n/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_globals_css__WEBPACK_IMPORTED_MODULE_1__);\n\n\nfunction App({ Component , pageProps  }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n        ...pageProps\n    }, void 0, false, {\n        fileName: \"/Users/igordolzhenkov/Projects/lokalise-prismic/src/pages/_app.tsx\",\n        lineNumber: 5,\n        columnNumber: 10\n    }, this);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvX2FwcC50c3guanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0FBQTZCO0FBR2QsU0FBU0EsSUFBSSxFQUFFQyxVQUFTLEVBQUVDLFVBQVMsRUFBWSxFQUFFO0lBQzlELHFCQUFPLDhEQUFDRDtRQUFXLEdBQUdDLFNBQVM7Ozs7OztBQUNqQyxDQUFDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbG9rYWxpc2UtcHJpc21pYy8uL3NyYy9wYWdlcy9fYXBwLnRzeD9mOWQ2Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnQC9zdHlsZXMvZ2xvYmFscy5jc3MnXG5pbXBvcnQgdHlwZSB7IEFwcFByb3BzIH0gZnJvbSAnbmV4dC9hcHAnXG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIEFwcCh7IENvbXBvbmVudCwgcGFnZVByb3BzIH06IEFwcFByb3BzKSB7XG4gIHJldHVybiA8Q29tcG9uZW50IHsuLi5wYWdlUHJvcHN9IC8+XG59XG4iXSwibmFtZXMiOlsiQXBwIiwiQ29tcG9uZW50IiwicGFnZVByb3BzIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/pages/_app.tsx\n");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ App)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _styles_GlobalStyles__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/GlobalStyles */ \"./src/styles/GlobalStyles.tsx\");\n\n\nfunction App({ Component , pageProps  }) {\n    return /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {\n        children: [\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(Component, {\n                ...pageProps\n            }, void 0, false, {\n                fileName: \"/Users/igordolzhenkov/Projects/lokalise-prismic/src/pages/_app.tsx\",\n                lineNumber: 6,\n                columnNumber: 5\n            }, this),\n            /*#__PURE__*/ (0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(_styles_GlobalStyles__WEBPACK_IMPORTED_MODULE_1__[\"default\"], {}, void 0, false, {\n                fileName: \"/Users/igordolzhenkov/Projects/lokalise-prismic/src/pages/_app.tsx\",\n                lineNumber: 7,\n                columnNumber: 5\n            }, this)\n        ]\n    }, void 0, true);\n}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvcGFnZXMvX2FwcC50c3guanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFDZ0Q7QUFFakMsU0FBU0MsSUFBSSxFQUFFQyxVQUFTLEVBQUVDLFVBQVMsRUFBWSxFQUFFO0lBQzlELHFCQUFROzswQkFDTiw4REFBQ0Q7Z0JBQVcsR0FBR0MsU0FBUzs7Ozs7OzBCQUN4Qiw4REFBQ0gsNERBQVdBOzs7Ozs7O0FBRWhCLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9sb2thbGlzZS1wcmlzbWljLy4vc3JjL3BhZ2VzL19hcHAudHN4P2Y5ZDYiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHR5cGUgeyBBcHBQcm9wcyB9IGZyb20gJ25leHQvYXBwJ1xuaW1wb3J0IEdsb2JhbFN0eWxlIGZyb20gJy4uL3N0eWxlcy9HbG9iYWxTdHlsZXMnXG5cbmV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIEFwcCh7IENvbXBvbmVudCwgcGFnZVByb3BzIH06IEFwcFByb3BzKSB7XG4gIHJldHVybiAoPD5cbiAgICA8Q29tcG9uZW50IHsuLi5wYWdlUHJvcHN9IC8+XG4gICAgPEdsb2JhbFN0eWxlIC8+XG4gICAgPC8+KVxufVxuIl0sIm5hbWVzIjpbIkdsb2JhbFN0eWxlIiwiQXBwIiwiQ29tcG9uZW50IiwicGFnZVByb3BzIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/pages/_app.tsx\n");
 
 /***/ }),
 
-/***/ "./src/styles/globals.css":
-/*!********************************!*\
-  !*** ./src/styles/globals.css ***!
-  \********************************/
-/***/ (() => {
+/***/ "./src/styles/GlobalStyles.tsx":
+/*!*************************************!*\
+  !*** ./src/styles/GlobalStyles.tsx ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ \"styled-components\");\n/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_0__);\n\nconst GlobalStyle = styled_components__WEBPACK_IMPORTED_MODULE_0__.createGlobalStyle`\n  body {\n    margin: 0;\n    padding: 0;\n    background: white;\n    font-family: Open-Sans, Helvetica, Sans-Serif;\n  }\n`;\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (GlobalStyle);\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvc3R5bGVzL0dsb2JhbFN0eWxlcy50c3guanMiLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQXNEO0FBRXRELE1BQU1DLGNBQWNELGdFQUFpQixDQUFDOzs7Ozs7O0FBT3RDLENBQUM7QUFFRCxpRUFBZUMsV0FBV0EsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL2xva2FsaXNlLXByaXNtaWMvLi9zcmMvc3R5bGVzL0dsb2JhbFN0eWxlcy50c3g/M2ZiMyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBjcmVhdGVHbG9iYWxTdHlsZSB9IGZyb20gJ3N0eWxlZC1jb21wb25lbnRzJztcbiBcbmNvbnN0IEdsb2JhbFN0eWxlID0gY3JlYXRlR2xvYmFsU3R5bGVgXG4gIGJvZHkge1xuICAgIG1hcmdpbjogMDtcbiAgICBwYWRkaW5nOiAwO1xuICAgIGJhY2tncm91bmQ6IHdoaXRlO1xuICAgIGZvbnQtZmFtaWx5OiBPcGVuLVNhbnMsIEhlbHZldGljYSwgU2Fucy1TZXJpZjtcbiAgfVxuYDtcbiBcbmV4cG9ydCBkZWZhdWx0IEdsb2JhbFN0eWxlOyJdLCJuYW1lcyI6WyJjcmVhdGVHbG9iYWxTdHlsZSIsIkdsb2JhbFN0eWxlIl0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/styles/GlobalStyles.tsx\n");
 
 /***/ }),
 
@@ -39,8 +39,17 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \****************************************/
 /***/ ((module) => {
 
-"use strict";
 module.exports = require("react/jsx-dev-runtime");
+
+/***/ }),
+
+/***/ "styled-components":
+/*!************************************!*\
+  !*** external "styled-components" ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = require("styled-components");
 
 /***/ })
 
---
+++ src/pages/api/get-translations.ts
@@ -0,0 +1,142 @@
+import { LokaliseApi, Language } from "@lokalise/node-api";
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as https from "https";
+import JSZip from "jszip";
+import fetch from "node-fetch";
+import AdmZip from "adm-zip";
+
+// const getZipFile = async (fileUrl: string) => {
+//   const data = await fetch(fileUrl) // 1) fetch the url
+//     .then(function (response) {
+//       // 2) filter on 200 OK
+//       console.log(response);
+//       if (response.status === 200 || response.status === 0) {
+//         return Promise.resolve(response.blob());
+//       } else {
+//         return Promise.reject(new Error(response.statusText));
+//       }
+//     });
+
+//   const zip = await JSZip.loadAsync(data);
+//   return zip;
+
+//   return new Promise((resolve, reject) => {
+//     const req = https.get(fileUrl, async (res) => {
+//       const data = [] as any;
+//       let totalDataLength = 0;
+
+//       //   res
+//       //     .on("data", (chunk) => {
+//       //       data.push(chunk);
+//       //       totalDataLength += chunk.length;
+//       //     })
+//       //     .on("end", async () => {
+//       //       try {
+//       //         const buf = Buffer.alloc(totalDataLength);
+//       //         for (let i = 0, len = data.length, pos = 0; i < len; i++) {
+//       //           data[i].copy(buf, pos);
+//       //           pos += data[i].length;
+//       //         }
+
+//       //         const zip = await JSZip.loadAsync(res);
+//       //         resolve(zip);
+//       //       } catch (e) {
+//       //         reject(e);
+//       //       }
+//       //     });
+//       // });
+
+//       const req = https.get(fileUrl, async (response) => {
+//         console.log(response);
+//         //const zip = await JSZip.loadAsync(response.);
+//         resolve("hello");
+//       });
+
+//       req.on("error", (err) => {
+//         reject(err);
+//       });
+//     });
+//   });
+// };
+
+const getZipFile = (fileUrl: string): Promise<AdmZip> => {
+  return new Promise((resolve, reject) => {
+    const req = https.get(fileUrl, async (res) => {
+      const data = [] as any;
+      let totalDataLength = 0;
+
+      res
+        .on("data", (chunk) => {
+          data.push(chunk);
+          totalDataLength += chunk.length;
+        })
+        .on("end", async () => {
+          try {
+            const buf = Buffer.alloc(totalDataLength);
+            for (let i = 0, len = data.length, pos = 0; i < len; i++) {
+              data[i].copy(buf, pos);
+              pos += data[i].length;
+            }
+
+            const zip = new AdmZip(buf);
+            resolve(zip);
+          } catch (e) {
+            reject(e);
+          }
+        });
+    });
+
+    req.on("error", (err) => {
+      reject(err);
+    });
+  });
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const translations: any[] = [];
+  const params = req.query;
+  console.log("params", params);
+  const languageParam = params.language;
+  const projectId = params.projectId;
+
+  const lokalise = new LokaliseApi({
+    apiKey: params.token as string,
+  });
+  if (!projectId) {
+    return;
+  }
+  const file = await lokalise.files().download(projectId as string, {
+    format: "json",
+    filter_langs: [languageParam as string],
+    export_sort: "first_added",
+    replace_breaks: false,
+    original_filenames: false,
+    export_empty_as: "skip",
+  });
+  console.log("file", file);
+
+  const zipUrl = file.bundle_url;
+  const zip = await getZipFile(zipUrl);
+  const zipEntries = zip.getEntries();
+
+  for (let i = 0; i < zipEntries.length; i++) {
+    if (zipEntries[i].entryName.match(/[a-z]*\.[a-z]*$/)) {
+      try {
+        translations.push({
+          file: zipEntries[i].entryName,
+          translations: JSON.parse(zip.readAsText(zipEntries[i])),
+        });
+        //writeFileRecursive(zipEntries[i].entryName, zip.readAsText(zipEntries[i]))
+        console.log(`SUCCESS for ${zipEntries[i].entryName}`);
+      } catch (err) {
+        console.log(`ERROR for ${zipEntries[i].entryName}`);
+        console.log(err);
+      }
+    }
+  }
+
+  res.status(200).json({ response: translations });
+}
