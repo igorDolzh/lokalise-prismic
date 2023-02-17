cc63475f49b40a363e4589d7ae491da1c770d165
Igor Dolzhenkov <igordolzhenkov@Igors-MacBook-Pro-3.local>
2023-02-17 14:41:10 +0300

    Save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,29 @@
 import Link from 'next/link'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
 
 export default function Home() {
   return (
-    <ul>
-      <li>
-        <Link href="/apply-recent-translations">Apply Recent Translations</Link>
-      </li>
-      <li>
-        <Link href="/export-new-messages">Export New Messages</Link>
-      </li>
-    </ul>
+    <div>
+      <h2>Prismic-Localise Integration</h2>
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/apply-recent-translations" >
+              <ListItemText primary="Apply Recent Translations" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="/export-new-messages">
+              <ListItemText primary="Export New Messages" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+    </div>
   )
 }
